a2c1972f2562bba2f5751b0e3a913d6faefb2ee1
yabetsg <yabets.g2@gmail.com>
2023-09-06 19:00:27 -0400

    Completed implementation of convert4To10 and convert10To4 methods in the conversion class

--- src/main/java/conversion.java
+++ src/main/java/conversion.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class conversion {
-    public int convert4To10(int i) throws inputBoundException{
-        return 0;
+    public int convert4To10(int quaternaryNumber) throws inputBoundException{
+        String inputToString = Integer.toString(quaternaryNumber);
+        String[] inputToArray = inputToString.split("");
+
+        if(inputToArray.length>5){
+            throw new inputBoundException("Invalid Input");
+        }
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i=inputToArray.length-1; i>=0; i--){
+            String digit = inputToArray[i];
+
+            if(!(digit.equals("0")||digit.equals("1")||digit.equals("2")||digit.equals("3"))){
+                throw new inputBoundException("Invalid Quaternary Number");
+            }
+
+            sum += (int) (Integer.parseInt(inputToArray[i]) * Math.pow(4,count));
+            count++;
+        }
+
+        return sum;
     }
 
-    public int convert10To4(int i) throws inputBoundException{
-        return 0;
+    public int convert10To4(int decimalNumber) throws inputBoundException{
+        if((decimalNumber<0 || Integer.toString(decimalNumber).split("").length>5)){
+            throw new inputBoundException("Invalid Input");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int quotient = decimalNumber;
+
+        while(quotient != 0){
+            int remainder = quotient%4;
+            quotient = (int) Math.floor(quotient/4);
+            sb.append(remainder);
+        }
+
+        return Integer.parseInt(String.valueOf(sb.reverse()));
     }
+
+
 }
+
+
